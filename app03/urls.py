e98753ed230704e3ccb33e9643587507548a94fb
systemime <systemime@gmail.com>
2020-08-21 14:38:13 +0800

    项目重启

--- app03/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from app03.views import test
-
-
-urlpatterns = [
-    path("test/", test, name="test"),
-]
--- app04_websocket/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class App04WebsocketConfig(AppConfig):
-    name = 'app04_websocket'
