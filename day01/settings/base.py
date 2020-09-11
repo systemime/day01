62d685efba2ae7c10b071f54329c585aa3b1fd2d
systemime <systemime@gmail.com>
2020-09-11 13:07:31 +0800

    修复数据库路由错误

--- day01/settings/base.py
+++ day01/settings/base.py
@@ -132,7 +132,7 @@ DATABASES = {
     }
 }
 
-DATABASE_ROUTERS = ['day01.settings.db_router.Router']
+DATABASE_ROUTERS = ['day01.common.db_router.MasterSlaveDBRouter']
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
