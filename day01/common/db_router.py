7d07911a176cdd4a930bcd256079e024338f62be
systemime <systemime@gmail.com>
2020-09-11 11:26:20 +0800

    数据库路由完成

--- day01/common/db_router.py
+++ day01/common/db_router.py
@@ -8,7 +8,7 @@ apps_labels = [app.split('.')[0] if not app.startswith('django')
                for app in settings.INSTALLED_APPS]
 
 
-class MasterSlaveDBRouter(object):
+class MasterSlaveDBRouter:
     """数据库主从读写分离路由"""
 
     read_db_labels = ('slave1_db', 'slave2_db')
@@ -16,23 +16,25 @@ class MasterSlaveDBRouter(object):
 
     def db_for_read(self, model, **hints):
         """读数据库 and 分库分表
+        所有的读操作走从库
         """
         if model._meta.app_label in apps_labels:
-            db = random.choice(self.read_db_labels)
+            db = random.choice(self.read_db_labels)  # nosec
             return db
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
-        允许迁移
+        允许迁移，db必须为master
         """
         # db.startswith(app_label) 返回app_label开头的数据库
-        # mysql主从迁移数据库必须通过master
-        if app_label in apps_labels:
+        if app_label in apps_labels and db == 'master_db':
             return True
         return False
 
     def db_for_write(self, model, **hints):
-        """写数据库"""
+        """写数据库
+        写数据走master数据库
+        """
         return self.write_db_labels
 
     def allow_relation(self, obj1, obj2, **hints):
